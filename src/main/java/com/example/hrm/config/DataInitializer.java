714cf76baec298e1e8646c67cba11af5d7edf1c6
thaoht204 <thao204hihi@gmail.com>
2026-04-01 23:28:20 +0700

    add docker file

--- src/main/java/com/example/hrm/config/DataInitializer.java
+++ src/main/java/com/example/hrm/config/DataInitializer.java
@@ -1,32 +1,40 @@
 package com.example.hrm.config;
 
 import com.example.hrm.entity.Account;
+import com.example.hrm.entity.Role;
 import com.example.hrm.repository.AccountRepository;
+import com.example.hrm.repository.RoleRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
-
 @Component
 @RequiredArgsConstructor
 public class DataInitializer implements CommandLineRunner {
 
     private final AccountRepository accountRepository;
+    private final RoleRepository roleRepository;
     private final PasswordEncoder passwordEncoder;
 
     @Override
     public void run(String... args) {
 
-        if (accountRepository.count() == 0) {
+        System.out.println("INIT DATABASE...");
+
+        if(accountRepository.findByUsername("admin").isEmpty()){
+
+            Role adminRole = roleRepository.findByName("ADMIN")
+                    .orElseThrow(() ->
+                            new RuntimeException("ADMIN role chưa tồn tại"));
 
             Account admin = new Account();
             admin.setUsername("admin");
-
-            admin.setPassword(
-                    passwordEncoder.encode("123456")
-            );
+            admin.setPassword(passwordEncoder.encode("123456"));
+            admin.setRole(adminRole); // ✅ QUAN TRỌNG
 
             accountRepository.save(admin);
+
+            System.out.println("ADMIN CREATED");
         }
     }
 }
