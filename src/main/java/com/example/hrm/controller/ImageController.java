52250715b66f4ee9c9d57eeea01f949e097929fe
thaoht204 <thao204hihi@gmail.com>
2026-04-04 21:36:15 +0700

    fix

---
+++ src/main/java/com/example/hrm/controller/ImageController.java
@@ -0,0 +1,22 @@
+package com.example.hrm.controller;
+
+import com.example.hrm.service.upload.ImageService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+@RestController
+@RequestMapping("/images")
+@RequiredArgsConstructor
+public class ImageController {
+
+    private final ImageService imageService;
+
+    @PostMapping("/upload")
+    public String upload(@RequestParam("file") MultipartFile file) {
+        return imageService.uploadImage(file);
+    }
+}
---
+++ src/main/java/com/example/hrm/service/upload/ImageService.java
@@ -0,0 +1,31 @@
+package com.example.hrm.service.upload;
+
+import com.cloudinary.Cloudinary;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import com.cloudinary.utils.ObjectUtils;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.Map;
+
+@Service
+@RequiredArgsConstructor
+public class ImageService {
+
+    private final Cloudinary cloudinary;
+
+    public String uploadImage(MultipartFile file) {
+        try {
+            Map uploadResult = cloudinary.uploader().upload(
+                    file.getBytes(),
+                    ObjectUtils.emptyMap()
+            );
+
+            return uploadResult.get("secure_url").toString();
+
+        } catch (IOException e) {
+            throw new RuntimeException("Upload failed");
+        }
+    }
+}
